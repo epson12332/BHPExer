2d4e5554e52f2d2dc24acec5f7b8d602cce9808d
charles <epson12332@gmail.com>
2017-05-19 11:32:43 +0800

    on mac

--- CH2/bhpnet.py
+++ CH2/bhpnet.py
@@ -19,5 +19,8 @@ port                = 0
 def usage():
     print "BHP Net Tool"
     print
-    print "Usage:bhpnet.py -t target_host -p port"
-    print "-l --listen         "
+    print "用法: bhpnet.py -t target_host -p port"
+    print "-l --listen               -在[host]:[port]監聽連入連線"
+    print "-e --execute=file_to_run  -接到連線時執行指定檔案"
+    print "-c --command              -啟動命令列shell "
+    print "-u --upload=destination   -接到連線時上傳檔案並寫出[destination]"
